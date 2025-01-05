be136a86504559f085129063874ddbd214026052
Willy N. Lúcio <willnaresse@gmail.com>
2025-01-05 13:30:29 -0300

    Ajuste responsividade ao editar conta e regex cadastro email

--- src/app/shared/validators/custom-email.validator.ts
+++ src/app/shared/validators/custom-email.validator.ts
@@ -1,14 +1,17 @@
 // src/app/shared/validators/custom-email.validator.ts
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-export const CustomEmailValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+export const CustomEmailValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
   const value = control.value;
 
   if (!value) {
     return null;
   }
 
-  const emailRegex = /^[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9-]{3,}\.(com|br)$/;
+  const emailRegex =
+    /^[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9-]{3,}\.(com|br|com\.br)$/;
 
   return emailRegex.test(value) ? null : { invalidEmail: true };
 };
