270aa832fb02c20a5271ac2ac9620f28e2fb676a
Willy Naresse Lucio <willy.lucio@unitri.edu.br>
2025-01-08 16:19:07 -0300

    build do projeto para Android usando capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vzinho.app',
+  appName: 'VZinho',
+  webDir: 'dist/project-checklist/browser'
+};
+
+export default config;
